3adcca920da7b9bd812d333e59171f9ebff056d0
Carlo Bilbao <carlo@Carlos-MacBook-Pro.local>
2019-04-04 14:14:55 -0700

    fix recursive call logic

--- src/utils/filters.js
+++ src/utils/filters.js
@@ -5,6 +5,21 @@
  * @param {object} allItems - an item object filled with items
  * @returns {object} - the root parent item
  */
+export const getRootParentItem = (childItem, allItems) => {
+    for(let itemKey in allItems) {
+        if(itemKey === childItem.parent) {
+            if(allItems[itemKey].parent === 'root') return allItems[itemKey];
+            else return getRootParentItem(allItems[itemKey], allItems);
+        }
+    }
+}
+
+/**
+ * Filters for the parent item of child Item.
+ * @constructor
+ * @param {object} allItems - an item object filled with items
+ * @returns {object} - the parent item
+ */
 export const getParentItem = (childItem, allItems) => {
     for(let itemKey in allItems) {
         if(itemKey === childItem.parent) {
@@ -23,13 +38,19 @@ export const getOwnerlessItems = allItems => {
     const ownerlessItems = []
     Object.keys(allItems).map(itemKey => {
         if(allItems[itemKey].owner === 'none' || allItems[itemKey].owner === 'shared') {
+            let rootParentItem = 'none';
             let parentItem = 'none';
-            if(allItems[itemKey].parent !== 'root') parentItem = getParentItem(allItems[itemKey], allItems);
+            if(allItems[itemKey].parent !== 'none') {
+                rootParentItem = getRootParentItem(allItems[itemKey], allItems);
+                parentItem = getParentItem(allItems[itemKey], allItems);
+            }
             ownerlessItems.push({
                 id: itemKey, 
                 data: allItems[itemKey], 
+                rootParentItem: rootParentItem, 
                 parentItem: parentItem, 
-                parentItemId: allItems[itemKey].parent});
+                parentItemId: allItems[itemKey].parent
+            });
         }
     });
     return ownerlessItems;
@@ -45,13 +66,19 @@ export const getUserItems = (allItems, uid) => {
     const userItems = [];
     Object.keys(allItems).map(itemKey => {
         if(allItems[itemKey].owner === uid) {
+            let rootParentItem = 'none';
             let parentItem = 'none';
-            if(allItems[itemKey].parent !== 'none') parentItem = getParentItem(allItems[itemKey], allItems);
+            if(allItems[itemKey].parent !== 'none') {
+                rootParentItem = getRootParentItem(allItems[itemKey], allItems);
+                parentItem = getParentItem(allItems[itemKey], allItems);
+            }
             userItems.push({
                 id: itemKey, 
                 data: allItems[itemKey], 
+                rootParentItem: rootParentItem,
                 parentItem: parentItem, 
-                parentItemId: allItems[itemKey].parent});
+                parentItemId: allItems[itemKey].parent
+            });
         }
     });
     return userItems;
@@ -60,13 +87,19 @@ export const getUserItems = (allItems, uid) => {
 export const getChildItems = (allItems, parentItem) => {
     const childItems = [];
     parentItem.children.forEach(childItemKey => {
+        let rootParentItem = 'none';
         let parentItem = 'none';
-        if(allItems[childItemKey].parent !== 'none') parentItem = getParentItem(allItems[childItemKey], allItems);
+        if(allItems[childItemKey].parent !== 'none') {
+            rootParentItem = getRootParentItem(allItems[childItemKey], allItems);
+            parentItem = getParentItem(allItems[childItemKey], allItems);
+        }
         childItems.push({
             id: childItemKey, 
             data: allItems[childItemKey], 
+            rootParentItem: rootParentItem,
             parentItem: parentItem, 
-            parentItemId: allItems[childItemKey].parent});
+            parentItemId: allItems[childItemKey].parent 
+        });
     })
     return childItems;
 }
